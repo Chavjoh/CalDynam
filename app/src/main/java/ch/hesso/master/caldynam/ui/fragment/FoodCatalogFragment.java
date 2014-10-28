f313de999b1dfad09141927015c89bb7fa5d3b7c
Chavjoh <j.chavaillaz@gmail.com>
2014-10-28 17:00:57 +0100

    Moved fragments to new package.

---
+++ app/src/main/java/ch/hesso/master/caldynam/ui/fragment/FoodCatalogFragment.java
@@ -0,0 +1,86 @@
+package ch.hesso.master.caldynam.ui.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ch.hesso.master.caldynam.MainActivity;
+import ch.hesso.master.caldynam.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FoodCatalogFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FoodCatalogFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class FoodCatalogFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment FoodCatalog.
+     */
+    public static FoodCatalogFragment newInstance() {
+        FoodCatalogFragment fragment = new FoodCatalogFragment();
+        return fragment;
+    }
+
+    public FoodCatalogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_catalog, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+
+        ((MainActivity) activity).onSectionAttached(R.string.section_food_catalog);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html">
+     * Communicating with Other Fragments</a> for more information.
+     * </p>
+     */
+    public interface OnFragmentInteractionListener {
+
+    }
+
+}
